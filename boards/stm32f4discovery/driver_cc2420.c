de73983a0f06d8889b9d70826893cdf905d172b7
Marco Scholtyssek <marco.scholtyssek056@stud.fh-dortmund.de>
2014-11-08 00:01:40 +0100

    driver implementation for cc2420 radio started

---
+++ boards/stm32f4discovery/driver_cc2420.c
@@ -0,0 +1,58 @@
+#include "cc2420_arch.h"
+#include "periph_conf.h"
+#include "stm32f407xx.h"
+#include "periph/spi.h"
+#include "hwtimer.h"
+#include "radio_driver.h"
+
+#define CC2420_CS_LOW       GPIO_0_PORT->ODR &= ~(1 << GPIO_0_PIN); 	/* PA0 */
+#define CC2420_CS_HIGH     	GPIO_0_PORT->ODR |= (1 << GPIO_0_PIN); 		/* PA0 */
+#define CC2420_RESET_PORT	GPIOA
+#define CC2420_RESET_PIN	(1 << 1)									/* PA1 */
+
+void cc2420_spi_init(void){
+	spi_init_master(SPI_0, SPI_CONF_FIRST_RISING, SPI_SPEED_5MHZ);
+}
+
+uint8_t cc2420_get_cca(void){
+	// TODO
+	return 0;
+}
+
+void core_panic(int crash_code, const char *message){
+	// TODO
+}
+
+void cc2420_init_interrupts(void){
+	// TODO
+}
+
+uint8_t cc2420_get_sfd(void){
+	// TODO
+	return 0;
+}
+
+uint8_t cc2420_get_fifop(void){
+	// TODO
+	return 0;
+}
+
+void cc2420_spi_select(void){
+	CC2420_CS_LOW
+}
+
+void cc2420_spi_unselect(void){
+	CC2420_CS_HIGH
+}
+
+void cc2420_reset(void){
+	CC2420_RESET_PORT->BSRRH |= CC2420_RESET_PIN;	// low
+	hwtimer_wait(500);
+	CC2420_RESET_PORT->BSRRL |= CC2420_RESET_PIN;	// high
+}
+
+uint8_t cc2420_txrx(uint8_t c){
+	char buf = '\0';
+	spi_transfer_byte(SPI_0, c, &buf);
+	return buf;
+}
