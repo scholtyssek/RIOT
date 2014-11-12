9788958ab19c5ecb6d022a4c644bea84c52a1594
Marco Scholtyssek <marco.scholtyssek056@stud.fh-dortmund.de>
2014-11-12 10:21:07 +0100

    CC2420 Treiber IO Struktur festgelegt und notwendige Funktion erstellt.

--- boards/stm32f4discovery/driver_cc2420.c
+++ boards/stm32f4discovery/driver_cc2420.c
@@ -1,13 +1,20 @@
 /*
  * This is the driver implementation for the cc2420 radio module.
- * The configuration is:
+ * The IO configuration is:
  * 	10 MHz spi clock
  * 	PA0 - CS
- * 	PA1 -
- * 	PA2 -
- * 	PA3 -
- * 	PA4 -
- * 	PA5 -
+ * 	PA1 - FIFO TODO
+ * 	PA2 - CCA TODO
+ * 	PA3 - SFD TODO
+ * 	PA4 - RESETN TODO
+ * 	PA5 - SCK
+ * 	PA6 - SO
+ *	PA7 - SI
+ *	PA8 - FIFOP TODO
+ *
+ *	Not connected:
+ *	VRegEn
+ *
  */
 
 #include "cc2420_arch.h"
@@ -21,10 +28,12 @@
 #define CC2420_CS_HIGH     	GPIO_0_PORT->ODR |= (1 << GPIO_0_PIN); 		/* PA0 */
 #define CC2420_RESET_PORT	GPIOA
 #define CC2420_CS_PIN		0											/* PA0 */
-#define CC2420_RESET_PIN	(1 << 1)									/* PA1 */
+#define CC2420_FIFO_PIN		(1 << 1)									/* PA1 */
 #define CC2420_CCA_PIN		(1 << 2)									/* PA2 */
 #define CC2420_SFD_PIN		(1 << 3)									/* PA3 */
-#define CC2420_FIFOP_PIN	(1 << 4)									/* PA4 */
+#define CC2420_RESET_PIN	(1 << 4)									/* PA4 */
+
+#define CC2420_FIFOP_PIN	(1 << 8)									/* PA8 */
 
 void cc2420_spi_init(void){
 	/* configure necessary pin */
@@ -35,14 +44,6 @@ void cc2420_spi_init(void){
 
 uint8_t cc2420_get_cca(void){
 	return GPIO_0_PORT->ODR & CC2420_CCA_PIN;
-}
-
-void core_panic(int crash_code, const char *message){
-	// TODO
-}
-
-void cc2420_init_interrupts(void){
-	// TODO
 }
 
 uint8_t cc2420_get_sfd(void){
@@ -71,4 +72,36 @@ uint8_t cc2420_txrx(uint8_t c){
 	char buf = '\0';
 	spi_transfer_byte(SPI_0, c, &buf);
 	return buf;
+}
+
+
+void core_panic(int crash_code, const char *message){
+	// TODO
+}
+
+void cc2420_init_interrupts(void){
+	// TODO
+}
+
+void cc2420_gdo0_enable(void){
+	// TODO
+}
+
+void cc2420_gdo0_disable(void){
+	// TODO
+}
+
+void cc2420_gdo2_enable(void){
+	// TODO
+}
+void cc2420_gdo2_disable(void){
+	// TODO
+}
+
+void cc2420_before_send(void){
+	// TODO disable interrupts before send. see header file
+}
+
+void cc2420_after_send(void){
+	// TODO reenable interrupts after send. see header file
 }
